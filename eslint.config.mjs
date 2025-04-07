326e280c890aff65011a654e5c80b71cbe6d1a15
Oskar Gierszewski <gierszon10@gmail.com>
2025-04-07 22:25:00 +0200

    Update ESLint configuration, add github actions workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,14 +16,9 @@ const compat = new FlatCompat({
 export default [
   ...compat.extends(
     'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/flowtype',
-    'prettier/react',
-    'prettier/standard'
+    'prettier'
   ),
   {
     languageOptions: {
